bc3f15c1867447f488b602bfbacbfa5093f8ec6f
Nicolas Bigler <nicolas.bigler@vshn.ch>
2017-10-24 13:05:22 +0200

    Disable Debug logging

--- slf2/__init__.py
+++ slf2/__init__.py
@@ -10,7 +10,7 @@ from slf2.slfapi import SlfApi, SlfError
 from slf2.influxdbapi import InfluxDbApi
 
 LOG = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
